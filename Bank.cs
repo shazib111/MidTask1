3f3959ca7b364f55e3da33f237c819c661997091
MD.SHAZIB SHEIKH <66199263+shazib111@users.noreply.github.com>
2021-03-03 01:35:19 +0600

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTask
+{
+    class Bank
+    {
+        private string bankName;
+        private Account[] myBank = new Account[10];
+
+        public string BankName
+        {
+            get { return this.bankName; }
+            set { this.bankName = value; }
+        }
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    myBank[i].GenarateAccountNumber();
+                    Console.WriteLine("Enter Your Address:");
+                    Console.WriteLine("Road No, House No, City, Country:");
+                    myBank[i].Address.RoadNo = Console.ReadLine();
+                    myBank[i].Address.HouseNo = Console.ReadLine();
+                    myBank[i].Address.City = Console.ReadLine();
+                    myBank[i].Address.Country = Console.ReadLine();
+                    Console.WriteLine("New Account Created..");
+                    myBank[i].ShowAccountInformation();
+                    break;
+                }
+            }
+        }
+    }
+}
+            
