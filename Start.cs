bb646f6287c149c51f64a999234bc262244923d6
MD.SHAZIB SHEIKH <66199263+shazib111@users.noreply.github.com>
2021-03-03 02:39:31 +0600

    Add files via upload

---
+++ Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTask
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Bank b1 = new Bank();
+            b1.BankName = " Black & White";
+            Console.WriteLine("Welcome To {0} Bank...", b1.BankName);
+        }
+    }
+}
